eb975cad57caab3b52576fd890008a1ef69b7890
phucdee <120212dinhphuc@isneu.org>
2023-09-01 23:52:55 +0700

    1st version of AF cost ETL

--- AppsFlyer Cost ETL/load_af_cost_file_from_gcs_to_bigquery.py
+++ AppsFlyer Cost ETL/load_af_cost_file_from_gcs_to_bigquery.py
@@ -1,15 +1,13 @@
-from pathlib import Path
 from google.cloud import storage, bigquery
 from google.oauth2 import service_account
-from google.api_core.exceptions import NotFound
 from dotenv import load_dotenv
 import os
 import json
-from prefect import task, flow, get_run_logger
 import datetime
 import re
-# from prefect.blocks.notifications import SlackWebhook
-import pendulum
+from prefect import task, flow, get_run_logger
+from prefect.blocks.notifications import SlackWebhook
+
 
 
 '''
@@ -20,23 +18,23 @@ import pendulum
 '''
 
 load_dotenv() # take environment variables from .env file for local testing
-# logger = get_run_logger() # setup a logger instance from prefect.io
 
-# """ basic task for slack noti"""
-# @task(name = "Slack Noti")
-# def slack_noti(text_input):
-#     slack_webhook_block = SlackWebhook.load("appsflyer")
-#     slack_webhook_block.notify(f"Flow {text_input}")
 
-# @flow(flow_run_name="AppsFlyer-cost-on-{date:%Y-%m-%d-%H-%M-%S}",
-#       timeout_seconds=3600,
-#       retries=1,
-#       retry_delay_seconds=10 
-# )
+""" basic task for slack noti"""
+@task(name = "Slack Noti")
+def slack_noti(text_input):
+    slack_webhook_block = SlackWebhook.load("appsflyer")
+    slack_webhook_block.notify(f"Flow {text_input}")
+
+@flow(flow_run_name="AppsFlyer-cost-on-{date:%Y-%m-%d-%H-%M-%S}",
+      timeout_seconds=3600,
+      retries=1,
+      retry_delay_seconds=10 
+)
 def load_af_cost_file_from_gcs_to_bigquery(ServiceAccountEnv=os.getenv('SERVICE_ACCOUNT'),
                          BucketName=os.getenv('BUCKET_NAME'),
                          ProjectId=os.getenv('PROJECT_ID'),
-                         Dataset=os.getenv('DATASET'),
+                         DatasetId=os.getenv('DATASET_ID'),
                          date=datetime.datetime.now()
     ):
     
@@ -44,6 +42,7 @@ def load_af_cost_file_from_gcs_to_bigquery(ServiceAccountEnv=os.getenv('SERVICE_
     # If you dont want to load info from .env file, you can load service account directly from your local file as 
     # json.load(open(r'your-file-path.json'))
     
+    logger = get_run_logger() # setup a logger instance from prefect.io, must stay inside a flow or a task of prefect
     credentials  = service_account.Credentials.from_service_account_info(json.loads(ServiceAccountEnv))
     bucket_name: str = BucketName.strip()
     folder_name: str = "cost_etl/v1"
@@ -53,35 +52,51 @@ def load_af_cost_file_from_gcs_to_bigquery(ServiceAccountEnv=os.getenv('SERVICE_
 
     ''' storage API'''
     bucket_client = storage.Client(credentials=credentials) # make api call to storage
-    print('call API good')
     list_files_in_bucket: list = bucket_client.list_blobs(bucket_or_name=bucket_name, prefix=folder_name) # get list file in storage
     list_file_4th_batch: list = [x.name for x in list_files_in_bucket if re.search(r"b=4", x.name)] 
+    logger.info('Check 4th batch in bucket')
 
     # if there is no 4th batch file, raise error and stop the flow
     if len(list_file_4th_batch) > 0:
-        print(len(list_file_4th_batch))
+        logger.info(f'Number of 4th batch file: {len(list_file_4th_batch)}')
     else:
         raise FileNotFoundError
     
-    # get list of date from 4th batch file
-    set_of_date_of_4th_batch_file: set = {re.search(r'(\d{4}-\d{2}-\d{2})', file).group() for file in list_file_4th_batch}
-    print(set_of_date_of_4th_batch_file)
+    # get list of date from 4th batch file by list comprehension and then turn it into a set to remove duplication. 
+    # After that using sorting fuction to make a list of date in decending order
+    list_of_date_of_4th_batch_file: list = sorted({re.search(r'(\d{4}-\d{2}-\d{2})', file).group() for file in list_file_4th_batch}, reverse=True)
 
     ''' Step 3: Check if there are existing tables of AF in bigquery'''
 
     '''bigquery API'''
     bqclient = bigquery.Client(credentials=credentials) # make api call to bigquery
-    dataset: str = f"{ProjectId}.{Dataset}" # set dataset 
+    dataset: str = f"{ProjectId}.{DatasetId}" # set dataset 
     list_tables_in_dataset: list = bqclient.list_tables(dataset=dataset)
 
     # check tables
     list_tables_in_dataset: list = [x.table_id for x in list_tables_in_dataset if re.search(r"appsflyer_cost_channel", x.table_id)]
-    print(list_tables_in_dataset)
+    logger.info('Check tables in bigquery')
 
-    ''' Step 4: If there is no table in bigquery, running full refresh - overwrite '''
-    if len(list_tables_in_dataset) == 0:
-        for date in set_of_date_of_4th_batch_file:
-            table_id = dataset + ".appsflyer_cost_channel_test_" + date
+    ''' Step 4: If there is no table in bigquery or number of date in 4th batch file smaller than 5 days, then running full refresh - overwrite, if not overwrite the latest 5 days tables '''
+    if len(list_tables_in_dataset) > 0 and len(list_of_date_of_4th_batch_file) >= 5:
+        # running incremental overwrite the last 5 days tables
+        logger.info('Run incremental overwrite the last 5 days tables')
+        for number in range(0,5):
+            table_id = dataset + ".appsflyer_cost_channel_" + list_of_date_of_4th_batch_file[number]
+            job_config = bigquery.LoadJobConfig(
+                source_format=bigquery.SourceFormat.PARQUET,
+                write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+            )
+            file_uri = f"gs://{bucket_name}/{folder_name}/dt={list_of_date_of_4th_batch_file[number]}/b=4/channel/*.parquet"
+
+            # Make an API request
+            load_job = bqclient.load_table_from_uri(file_uri, table_id, job_config=job_config)
+            load_job.result()  # Waits for the job to complete
+    else:
+        # running full refresh overwrite all tables
+        logger.info('Run full refresh overwrite all tables')
+        for date in list_of_date_of_4th_batch_file:
+            table_id = dataset + ".appsflyer_cost_channel_" + date
             job_config = bigquery.LoadJobConfig(
                 source_format=bigquery.SourceFormat.PARQUET,
                 write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
@@ -92,74 +107,13 @@ def load_af_cost_file_from_gcs_to_bigquery(ServiceAccountEnv=os.getenv('SERVICE_
             load_job = bqclient.load_table_from_uri(file_uri, table_id, job_config=job_config)
             load_job.result()  # Waits for the job to complete
 
-
-    # ''' bigquery credential'''
-    # bqclient = bigquery.Client(credentials=credentials)
-    # table_id = f"{ProjectId}.{Dataset}.{schema_id_from_adjust}" # set table id with schema hash from adjust
-
-    # schema_path = f"{cwd}/schema.json"
-
-    # schema = bqclient.schema_from_json(schema_path)
-
-    # try:
-    #     logger.info('RUN TRY')
-    #     destination_table = bqclient.get_table(table_id) # Make an API request.
-    #     logger.info(f'destination table row before append - {destination_table.num_rows} ')
-    #     logger.info(f' number of files to append - {len(list_latest_csv_file)}')
-    #     for f in list_latest_csv_file:
-    #         file_uri = f"gs://{bucket_name}/" + f"{f}"
-            
-    #         logger.info(f'append file - {f} ')
-    #         job_config = bigquery.LoadJobConfig(
-    #             schema=schema,
-    #             skip_leading_rows=1,
-    #             source_format=bigquery.SourceFormat.CSV,
-    #             write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
-    #             time_partitioning=bigquery.TimePartitioning(
-    #                 type_=bigquery.TimePartitioningType.DAY,
-    #                 field="_created_at_"
-    #                 ) 
-    #         )
-    #         # Make an API request.
-    #         load_job = bqclient.load_table_from_uri(
-    #             file_uri, table_id, job_config=job_config
-    #         )
-    #         load_job.result()  # Waits for the job to complete.
-    #     destination_table = bqclient.get_table(table_id) # Make an API request.
-    #     logger.info(f'destination table row after append- {destination_table.num_rows} ')
-
-    # # when table is not created, then run the below code
-    # except NotFound:
-    #     logger.info('RUN EXCEPT')
-    #     logger.info(f' number of files to append - {len(list_all_csv_file_with_latest_adjust_schema)}')
-    #     for f in list_all_csv_file_with_latest_adjust_schema:
-    #         file_uri = f"gs://{bucket_name}/" + f"{f}"
-    #         logger.info(f'append file - {f} ')
-    #         job_config = bigquery.LoadJobConfig(
-    #             schema=schema,
-    #             skip_leading_rows=1,
-    #             source_format=bigquery.SourceFormat.CSV,
-    #             write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
-    #             time_partitioning=bigquery.TimePartitioning(
-    #                 type_=bigquery.TimePartitioningType.DAY,
-    #                 field="_created_at_"
-    #                 ) 
-    #         )
-    #         # Make an API request.
-    #         load_job = bqclient.load_table_from_uri(
-    #             file_uri, table_id, job_config=job_config
-    #         )
-    #         load_job.result()  # Waits for the job to complete.
-    #     destination_table = bqclient.get_table(table_id)
-    #     logger.info(f'destination table row after append- {destination_table.num_rows} ')
-
-# @flow(flow_run_name="{AppName}-on-{date:%Y-%m-%d-%H-%M-%S}",
-#       timeout_seconds=3600  
-# )
-def main_flow(date=datetime.datetime.now()):
-    # slack_noti("Run")
+@flow(flow_run_name="AppsFlyer-cost-on-{date:%Y-%m-%d-%H-%M-%S}",
+      timeout_seconds=3600  
+)
+def AppsFlyer_cost_main_flow(date=datetime.datetime.now()):
+    slack_noti("Run load_af_cost_file_from_gcs_to_bigquery")
     load_af_cost_file_from_gcs_to_bigquery()
-    # slack_noti("Completed")
+    slack_noti("Completed load_af_cost_file_from_gcs_to_bigquery")
 
 if __name__ == "__main__":
-    main_flow()
+    AppsFlyer_cost_main_flow()
